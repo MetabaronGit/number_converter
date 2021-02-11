026e781c05521ade13d3abb6a1c71dfbc68b8b1e
rad <cepelka.jr@email.cz>
2021-02-11 15:32:43 +0100

    clean code

--- main.py
+++ main.py
@@ -3,21 +3,19 @@
 roman_numbers = ("I", "V", "X", "L", "C", "D", "M")
 arabic_numbers = (1, 5, 10, 50, 100, 500, 1000)
 
+first_number = second_number = 0
+final_transfer = 0
+
 roman_string = str(input("Zadej římské číslo: ")).upper()
 
-firstNumber = secondNumber = 0
-finalTransfer = 0
-
 for letter in roman_string:
-    print(letter)
-    transferIndex = roman_numbers.index(letter)
-    firstNumber = secondNumber
-    secondNumber = arabic_numbers[transferIndex]
+    first_number = second_number
+    second_number = arabic_numbers[roman_numbers.index(letter)]
 
-    if (0 < firstNumber) and (firstNumber < secondNumber):
-        finalTransfer += firstNumber * (-1)
+    if (0 < first_number) and (first_number < second_number):
+        final_transfer += first_number * (-1)
     else:
-        finalTransfer += firstNumber
+        final_transfer += first_number
 
-finalTransfer += secondNumber
-print("Arabské číslo je : {0}".format(finalTransfer))
+final_transfer += second_number
+print("Arabské číslo je : {0}".format(final_transfer))

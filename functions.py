b8f47063f7a125d89915f0ce3cb15d6ea740abc7
MetabaronGit <76850166+MetabaronGit@users.noreply.github.com>
2021-04-03 19:55:26 +0200

    03-01

---
+++ functions.py
@@ -0,0 +1,26 @@
+
+
+
+def to_arab(roman_string: str):
+    roman_numbers = ("I", "V", "X", "L", "C", "D", "M")
+    arabic_numbers = (1, 5, 10, 50, 100, 500, 1000)
+    first_number = 0
+    second_number = 0
+    final_transfer = 0
+
+    for letter in roman_string:
+        first_number = second_number
+        second_number = arabic_numbers[roman_numbers.index(letter)]
+
+        if (0 < first_number) and (first_number < second_number):
+            final_transfer += first_number * (-1)
+        else:
+            final_transfer += first_number
+
+    final_transfer += second_number
+    return final_transfer
+
+def to_roman(arab_number: int):
+    pass
+
+print(to_arab("VIII"))
